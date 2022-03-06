e8089de32b8f3f9cf7cec233f3f07aa75b8747df
Felpson <felipe_a.almeida@outlook.com>
2022-03-06 14:08:43 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="main">
+        <div className="center">
+          <div className="menu">
+            <div className="logo">
+              <h3>Bem-vindo!</h3>
+            </div>
+            <div className="item-menu">
+              <a href="#">Login</a>
+            </div>
+          </div>
+          <div className="form">
+            <form>
+              <h2>
+                Entre em contato!
+              </h2>
+              <div className="items-form">
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="submit" />
+              </div>
+            </form>  
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
